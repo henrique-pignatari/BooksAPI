6cc4f12f8b9ed27bf7bc34c7d1a961f4812abd26
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-10-18 00:18:30 -0300

    feat: Add category validation and contructor

--- Books.Domain/Entities/Category.cs
+++ Books.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Books.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,11 @@ namespace Books.Domain.Entities
     {
         public string Name { get; private set; }
         public ICollection<Book> Books { get; private set; }
+
+        public Category(string name)
+        {
+            CategoryValidation.ValidateName(name);
+            Name = name;
+        }
     }
 }
---
+++ Books.Domain/ErrorMessages/CategoryErrorMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Domain.ErrorMessages
+{
+    public class CategoryErrorMessages
+    {
+        public const string NullOrEmpityName = "Invalid Name. Name is required.";
+        public const string ShortName = "Invalid Name. Name is too short.";
+        public const string LongName = "Invalid Name. Name is too long";
+    }
+}
