12defc1dc921471c7a399958f4c5aa45b6611726
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-10-18 00:18:55 -0300

    feat: add Genre class and its validation

---
+++ Books.Domain/Entities/Genre.cs
@@ -0,0 +1,21 @@
+﻿using Books.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Domain.Entities
+{
+    public class Genre : Entity
+    {
+        public string Name { get; private set; }
+        public ICollection<Book> Books { get; private set; }
+
+        public Genre(string name)
+        {
+            GenreValidation.ValidateName(name);
+            Name = name;
+        }
+    }
+}
