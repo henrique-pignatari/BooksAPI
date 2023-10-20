175cbd24809221021dbfa6a18907255846e7ed15
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-10-19 21:18:52 -0300

    feat: create repositories

---
+++ Books.Domain/Interfaces/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using Books.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Domain.Interfaces
+{
+    public interface IBookRepository : INamedEntityRepository<Book>
+    {
+        Task<IEnumerable<Book>> GetByAuthorAsync(int authotId, int? quantity, int? page);
+        Task<IEnumerable<Book>> GetByGenreAsync(int genreId, int? quantity, int? page);
+        Task<IEnumerable<Book>> GetByCategoryAsync(int categoryId, int? quantity, int? page);
+    }
+}
---
+++ Books.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Books.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetByIdAsync(int id);
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> RemoveAsync(T entity);
+    }
+}
