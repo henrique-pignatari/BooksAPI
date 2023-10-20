77bbd3a232d4719ac28ee3a660c39eef85db2b52
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-10-19 23:54:38 -0300

    feat: configure dependency injection

--- Books.API/Program.cs
+++ Books.API/Program.cs
@@ -1,3 +1,5 @@
+using Books.Infra.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructureSwagger();
+builder.Services.AddInfrastructureAPI(builder.Configuration);
 
 var app = builder.Build();
 
