b47a11a495470d8261e4fed705f23f8c7998ae59
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-10-25 18:00:50 -0300

    feat: add autho controller

---
+++ Books.API/Controllers/AuthorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+    }
+}
