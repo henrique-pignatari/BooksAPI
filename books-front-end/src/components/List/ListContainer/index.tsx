320c66ab0d510a911e993f36e6c9de888bd68f27
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-01-08 22:07:07 -0300

    feat: add pagination header

--- books-front-end/src/components/List/ListContainer/index.tsx
+++ books-front-end/src/components/List/ListContainer/index.tsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 import styles from "./styles.module.css";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
+import Image from "next/image";
+import singleArrow from "../../../assets/single-arrow.svg";
+import doubleArrow from "../../../assets/double-arrow.svg";
 
 type Props = {
   title: string;
@@ -19,7 +22,26 @@ const ListContainer = ({ title, children }: Props) => {
       <Link className={styles.createLink} href={`${pathname}/create`}>
         Novo
       </Link>
-      {children}
+      <div className={styles.listContainer}>
+        <div className={styles.listHeader}>
+          <div className={styles.paginationWrapper}>
+            <button className={`${styles.paginationButton} ${styles.arrowLeft}`}>
+              <Image src={singleArrow} alt="" />
+            </button>
+            <button className={`${styles.paginationButton} ${styles.arrowLeft}`}>
+              <Image src={doubleArrow} alt="" />
+            </button>
+
+            <button className={`${styles.paginationButton} ${styles.arrowRight}`}>
+              <Image src={doubleArrow} alt="" />
+            </button>
+            <button className={`${styles.paginationButton} ${styles.arrowRight}`}>
+              <Image src={singleArrow} alt="" />
+            </button>
+          </div>
+        </div>
+        {children}
+      </div>
     </section>
   );
 };
