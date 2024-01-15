079057c3a2765728c1bcfde8072b2e09e249a159
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-01-14 22:03:42 -0300

    feat: add genres list provider

---
+++ books-front-end/src/contexts/genresList.ts
@@ -0,0 +1,19 @@
+import { GENRE_URL } from "@/global/urls";
+import { listProviderFactory } from "./factories/listsFactory";
+import { useContext } from "react";
+
+type Genre = {
+  id: string;
+  name: string;
+}
+
+const {context, ProviderElement: GenresListProvider} = listProviderFactory<Genre>({
+  baseUrl: GENRE_URL
+})
+
+const useGenresList = () => useContext(context)
+
+export{
+  useGenresList,
+  GenresListProvider
+}
--- books-front-end/src/global/urls.ts
+++ books-front-end/src/global/urls.ts
@@ -2,3 +2,4 @@ const BASE_URL = "http://localhost:5000/api" //TODO change to environment
 export const BOOKS_URL = `${BASE_URL}/book`
 export const AUTHORS_URL = `${BASE_URL}/author`
 export const CATEGORIES_URL = `${BASE_URL}/category`
+export const GENRE_URL = `${BASE_URL}/genre`
